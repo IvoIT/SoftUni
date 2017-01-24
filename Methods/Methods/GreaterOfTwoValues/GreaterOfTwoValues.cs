f551a3ea5525962f2171825c0024a191331d7f66
IvoIT <aienngaming@gmail.com>
2017-01-24 20:11:46 +0200

    new changes

--- Methods/Methods/GreaterOfTwoValues/GreaterOfTwoValues.cs
+++ Methods/Methods/GreaterOfTwoValues/GreaterOfTwoValues.cs
@@ -7,6 +7,85 @@ namespace GreaterOfTwoValues
     {
         public static void Main()
         {
+            var type = Console.ReadLine();
+
+            if (type == "int")
+            {
+                int first = int.Parse(Console.ReadLine());
+                int second = int.Parse(Console.ReadLine());
+                int larger = GetIntMax(first, second);
+
+                Console.WriteLine(larger);
+            }
+
+            else if (type == "char")
+            {
+                char first = char.Parse(Console.ReadLine());
+                char second = char.Parse(Console.ReadLine());
+                char larger = GetCharMax(first, second);
+
+                Console.WriteLine(larger);
+            }
+
+            else
+            {
+                string first = Console.ReadLine();
+                string second = Console.ReadLine();
+                string larger = GetStringMax(first, second);
+
+                Console.WriteLine(larger);
+            }
+        }
+
+        public static int GetIntMax(int first, int second)
+        {
+            int larger = 0;
+
+            if (first >= second)
+
+            {
+                larger = first;
+            }
+
+            else
+            {
+                larger = second;
+            }
+
+            return larger;
+        }
+
+        public static char GetCharMax(char first, char second)
+        {
+            char larger = (char)0x00;
+
+            if (first >= second)
+            {
+                larger = first;
+            }
+
+            else
+            {
+                larger = second;
+            }
+
+            return larger;
+        }
+
+        public static string GetStringMax(string first, string second)
+        {
+            string larger = string.Empty;
+
+            if (first.CompareTo(second) >= 0)
+            {
+                larger = first;
+            }
+            else
+            {
+                larger = second;
+            }
+
+            return larger;
         }
     }
 }
---
+++ Methods/Methods/MultiplyEvensByOdd/MultiplyEvenByOdd.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace MultiplyEvensByOdd
+{
+    public class MultiplyEvenByOdd
+    {
+        public static void Main()
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+            int result = MultipliesOddByEvenNumbers(number);
+
+            Console.WriteLine(result);
+
+        }
+
+        public static int MultipliesOddByEvenNumbers(int number)
+        {
+            int evenSum = GetSumOfEvenDiggits(number);
+            int oddSum = GetSumOfOddDigits(number);
+
+            int result = evenSum * oddSum;
+
+            return result;
+        }
+
+        public static int GetSumOfOddDigits(int number)
+        {
+            int sum = 0;
+
+            while (number > 0)
+            {
+                int lastDigit = number % 10;
+                number = number / 10;
+
+                if (lastDigit % 2 != 0)
+                {
+                    sum += lastDigit;
+                }
+
+            }
+
+            return sum;
+        }
+
+        public static int GetSumOfEvenDiggits(int number)
+        {
+            int sum = 0;
+
+            while (number > 0)
+            {
+                int lastDigit = number % 10;
+                number = number / 10;
+
+                if (lastDigit % 2 == 0)
+                {
+                    sum += lastDigit;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
